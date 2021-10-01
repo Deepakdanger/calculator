b6df657ce120166d8bce6a067c5c41f8aa9b7634
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-01 16:08:50 +0530

    Add display in app for display result

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 function App() {
   return (
     <>
+      <Display />
       <ButtonPanel />
     </>
   );
