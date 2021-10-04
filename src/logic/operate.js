ffc5544646849ee82c606e72a7e4c3ee08a9df4f
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-04 19:11:29 +0530

    Add operate fuction

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,18 +1,18 @@
 import Big from 'big.js';
 
 const operate = (total, next, operation) => {
-  if (operation == '+') {
-    total += next;
-  } else if (operation == '-') {
-    total -= next;
-  } else if (operation == 'x') {
-    total *= next;
-  } else if (operation == '/') {
-    total /= next;
-  } else if (operation == '%') {
-    total /= 100;
-  }
-  return total;
-};
+    if (operation == '+') {
+      total += next;
+    } else if (operation == '-') {
+      total -= next;
+    } else if (operation == 'x') {
+      total *= next;
+    } else if (operation == '/') {
+      total /= next;
+    } else if (operation == '%') {
+      total /= 100;
+    }
+    return total;
+  };
 
 export default operate;
