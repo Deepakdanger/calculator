f9e957807229f66de11ff3202b983662fbe4fb85
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-05 13:35:19 +0530

    Add plus ,minus,times and div in operate.js file

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -4,17 +4,15 @@ const operate = (num1st, num2nd, operation) => {
   let num1 = new Big(num1st);
   let num2 = new Big(num2nd);
   if (operation === '+') {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    num2 += num1;
+    num2 = num2.plus(num1);
     num1 = num2;
   } else if (operation === '-') {
-    num2 -= num1;
+    num2 = num2.minus(num1);
     num1 = num2;
   } else if (operation === 'X') {
-    num1 *= num2;
+    num1 = num2.times(num1);
   } else if (operation === '/') {
-    num2 /= num1;
+    num2 = num2.div(num1);
     num1 = num2;
   }
   return num1.toString();
