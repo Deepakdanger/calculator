04bef463ebbf7f117ce553f28e8d0dcdc977e041
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-08 16:47:19 +0530

    Done Navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const Navbar = () => (
+  <nav className="nav1">
+    <p className="logo11">Math Magic</p>
+    <ul className="right">
+      <li><a href="/">Home</a></li>
+      <li><a href="/calculator">Calculator</a></li>
+      <li><a href="/quote">Quote</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
