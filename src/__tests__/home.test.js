62face3c151bf461ddba4a5170e251a99c73de8b
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-11 19:12:20 +0530

    Add snapshot test for home and quote

---
+++ src/__tests__/home.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from '../components/home';
+
+it('render home UI correctly', () => {
+  const home = renderer.create(<Home />).toJSON();
+  expect(home).toMatchSnapshot();
+});
