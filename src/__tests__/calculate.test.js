a1e081a368e42bc6df75ee0bcd6fe02c123aacca
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-11 18:05:06 +0530

    Add test about numbers checking and dicimal operation

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -8,4 +8,52 @@ describe('calculator logic', () => {
     expect(calc.next).toEqual('');
     expect(calc.operation).toEqual('');
   });
+
+  it('add if operation is equal to +', () => {
+    const calc = calculate({ total: 9, next: 4, operation: '+' }, '=');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('13');
+  });
+
+  it('subtract if operation is equal to -', () => {
+    const calc = calculate({ total: 4, next: 9, operation: '-' }, '=');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('5');
+  });
+
+  it('multiply if operation is equal to x', () => {
+    const calc = calculate({ total: 9, next: 4, operation: 'X' }, '=');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('36');
+  });
+
+  it('divide if operation is equal to ÷', () => {
+    const calc = calculate({ total: 3, next: 9, operation: '÷' }, '=');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('3');
+  });
+
+  it('decimal add with adding 0. if calcult is less than 1', () => {
+    const calc = calculate({ total: 0.5, next: 0.4, operation: '+' }, '=');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('0.9');
+  });
+
+  it('When a . is clicked, and total is not null, operation is null and next is null, it returns a total 12.', () => {
+    const calc = calculate({ total: '9', next: '', operation: '' }, '.');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('9.');
+  });
+
+  it('make number negative when name is equal to +/-', () => {
+    const calc = calculate({ total: 9, next: '', operation: '' }, '+/-');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('-9');
+  });
+
+  it('do modulo with 100 when name is equal to %', () => {
+    const calc = calculate({ total: 9, next: '', operation: '' }, '%');
+    expect(calc).toHaveProperty('total');
+    expect(calc.total).toEqual('0.09');
+  });
 });
