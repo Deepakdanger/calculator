058171e858e93f53d678fe360b85eecc0b44babc
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-11 14:33:48 +0530

    Add math quote

--- src/components/home.js
+++ src/components/home.js
@@ -4,9 +4,8 @@ import Navbar from '../navbar';
 const Home = () => (
   <div className="homes">
     <Navbar />
-    <h1>
-      Home
-    </h1>
+    <h1>Welcome to our Page!</h1>
+    <p className="para">This is a math magician website which provide calculator as well as inspiring quote.</p>
   </div>
 );
 export default Home;
--- src/components/quote.js
+++ src/components/quote.js
@@ -4,9 +4,11 @@ import Navbar from '../navbar';
 const Quote = () => (
   <div className="quotess">
     <Navbar />
-    <h1>
-      Quote
-    </h1>
+    <h1>Quote here!</h1>
+    <p className="para">
+      Math may not teach us how to ADD or SUBTRACT hate but it gives us hope that
+      EVERY PROBLEM has a SOLUTION . –Anonymous
+    </p>
   </div>
 );
 export default Quote;
